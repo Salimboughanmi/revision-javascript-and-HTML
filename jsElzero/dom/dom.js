2407a6b2bb74fb6bb975647c86ff05e5a7c875bd
salimboughanmi <boughanmi1salim@gmail.com>
2024-04-22 23:45:57 +0100

    commit3

--- jsElzero/dom/dom.js
+++ jsElzero/dom/dom.js
@@ -25,13 +25,43 @@ console.log(body) // afficher le body
 let title = document.title.replace ="kkkkkk"
 console.log(title)
  */
-//////******************************************* ajout et  modifier les attribut de html */
+//////******************************************* ajout et  modifier les attribut de html */********************************************** */
 //<element attribute > text </element>
 // get attributes :  afficher les attributes  
 //set attributes :ajouter ou modifier les attributes
 //remove attributes : afficher les attributes
 
-let image = document.getElementById("img")
+/* let image = document.getElementById("img")
 console.log(image.alt)
 console.log(image.src)
 console.log(image.className)
+// set attribute of element
+//image.src="../img/3.jpg" // change image src
+//console.log(image.hasAttribute ('alt'))// verify attribute false or true
+console.log(image.attributes)// ou avec [index]
+console.log(image.setAttribute ('alt',"salimmmm"))// change attribute
+image.removeAttribute ('alt') // remove attribute alt */
+//////******************************************* sibling- parent */**********************************************
+//element.nextElementSibling : Cette propriété renvoie le prochain élément frère (élément enfant du même parent) de l'élément spécifié dans le document DOM, dans l'ordre de leur apparition dans le code HTML.
+//element.previousElementSibling : Cette propriété renvoie l'élément frère précédent (élément enfant du même parent) de l'élément spécifié dans le document DOM, dans l'ordre de leur apparition dans le code HTML.
+//element.nextSibling : Cette propriété renvoie le nœud suivant dans l'arbre DOM, y compris les nœuds textuels, les commentaires et les éléments, à l'intérieur du même parent que l'élément spécifié.
+//element.previousSibling : Cette propriété renvoie le nœud précédent dans l'arbre DOM, y compris les nœuds textuels, les commentaires et les éléments, à l'intérieur du même parent que l'élément spécifié.
+//element.parentElement : Cette propriété renvoie l'élément parent de l'élément spécifié dans le document DOM.
+//Ces scripts sont très utiles lors de la manipulation et de la navigation dans le DOM (Document Object Model) en JavaScript. Ils permettent d'accéder aux éléments voisins et parent d'un élément donné dans la structure HTML du document.
+
+//////******************************************* innerHTML- outerHTML */**********************************************
+
+let container = document.getElementById('container');
+//console.log(container.outerHTML) // afficher tt les elemt sous elt d'id conatainer, expl : tt les elemt de div et les elmt sous elmt div
+//console.log(container.innerHTML) // afficher jst les elmt sous id conatainer ctt , l'elmt de id conatainer n'affiche pas
+//container.outerHTML ='<p> hellooooo</p>' // changer tt le div et le remplacer avec <p>
+container.innerHTML +='<p> hellooooo</p>' // ajouter un <p> dans le <div>
+container.innerHTML ='<p> hellooooo</p>' // ajouter un jst <p> dans le <div> et effacer les autre contenu de div
+container.innerText=kkkkkkkkkkkk
+console.log(container)
+
+
+
+
+
+
