6a4fa1e15e71e4d10fa3df9c81e36e4cab3f8432
salimboughanmi <boughanmi1salim@gmail.com>
2024-06-09 13:37:25 +0100

    add location

---
+++ jsElzero/BOM/location.js
@@ -0,0 +1,18 @@
+//https://www.w3schools.com/bootstrap/ ==href
+//https == protocole
+//www.w3schools.com/ ==hostname
+// /bootstrap/ == pathname
+
+//GET
+
+/* console.log(location.href)
+console.log(location.protocol)
+console.log(location.hostname)
+console.log(location.pathname) */
+
+// SET
+location.href = "https://www.w3schools.com/bootstrap/"
+/* location.protocol = "http:"
+location.hostname = "www.w3schools.com"
+location.pathname = "/bootstrap/" */
+location.href = "fb.com" // changer to another link
