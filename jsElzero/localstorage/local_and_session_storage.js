946207e5880453ad07c7f16690602201e425b8a3
salimboughanmi <boughanmi1salim@gmail.com>
2024-06-10 15:10:47 +0100

    add local storage and remove

--- jsElzero/localstorage/local_and_session_storage.js
+++ jsElzero/localstorage/local_and_session_storage.js
@@ -1,12 +1,39 @@
-
-//localStorage = data enregistrer dans domaine 
-
+// local storage = 5 MB
+//sessionStorage = 5 MB -10 MB
+//localStorage = data enregistrer dans domaine (url)
+// deux methode pour stacke localstorage
+////////////////////////////////////////////////////////////////////////////////set localStorage
 localStorage.setItem('name', "salim"); // key and value
-localStorage.lastname ='boughanmi'
+localStorage.lastname ='boughanmi' 
+localStorage.setItem('age',JSON.stringify(55)); // avec JSON.stringify SERA DE type number dans local storage
+localStorage.setItem('skills', [1,2,3,4]) // quand ns avons ajouter array value stcker sous forme string ====> faux
+localStorage.setItem('number' , JSON.stringify([11,22,23,24,])); // quand ns avons ajouter array value stcker sous forme arrayc====> vrai 
 
-//get localStorage
+//rq : on peut eliminer JSON.stringify QAND ON PEUT AJOUTER STRING
 
-console.log(localStorage.getItem('name'))
-console.log(localStorage.lastname)
+// ajouter object
+localStorage.user = JSON.stringify({  // aavec JSON.stringify object sera sous forme object dans local storage
+    name : "fedi",
+    lastname : "abidi",
+    age : 20
+})
+//ou 
+localStorage.setItem('user2', JSON.stringify({  // aavec JSON.stringify object sera sous forme object dans local storage
+    name : "semo",
+    lastname : "darragi",
+    age : 20
+}))
 
-localStorage.removeItem('name') // remove item by key
+// donc avec JSON.stringify TOUT les input sera sous form String
+//////////////////////////////////////////////////////////////////////////////get localStorage
+console.log(typeof
+    JSON.parse(localStorage.getItem('user'))) //avec JSON.parse get user avec sons type object
+
+console.log(localStorage.key(0))
+
+/////////////////////////////////////////////////////////////////////////////// remove localStorage
+
+localStorage.removeItem('number') // remove item by key
+
+localStorage.clear() // remove all item in local storage
+
