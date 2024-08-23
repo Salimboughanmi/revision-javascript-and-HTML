066979a35c6639a229ba53b4c15e609af4f22632
salimboughanmi <boughanmi1salim@gmail.com>
2024-08-23 17:14:12 +0100

    covert to json

---
+++ JSON/Parse And Stringify.js
@@ -0,0 +1,18 @@
+/* JSON
+ - JSON.parse=> convert text data to js Object
+ - JSON.stringify=> convert JS object to JSON */
+
+ const MyJsonObjectFromServer ='{"username" : "salim" ,"age":"30" }' ;
+ console.log(typeof MyJsonObjectFromServer) // type string
+ console.log(MyJsonObjectFromServer) // type string
+
+ const MyJsObject = JSON.parse(MyJsonObjectFromServer)
+ console.log(typeof MyJsObject) // type object
+ console.log(MyJsObject) // type object
+
+ MyJsObject["username"] = "sami"
+MyJsObject["age"] = "20"
+
+ //console.log(MyJsObject) 
+
+ const MyJsonObjectToServer =JSON.stringify(MyJsObject) // convert a javascrpt value to a javascript object notation (JSON)
