1b91f1e82d1baaa03e0016043fc4404f30e080ab
salimboughanmi <boughanmi1salim@gmail.com>
2024-08-25 22:16:47 +0100

    Higher Order Functions - reduce practice

--- jsElzero/function/HightOrderFCT2.js
+++ jsElzero/function/HightOrderFCT2.js
@@ -1,6 +1,6 @@
 console.log("***********************************  Higher Order Functions -  Reduce **************************************")
 
- let nums =[10 , 15 ,20 , 30 ]
+ /* let nums =[10 , 15 ,20 , 30 ]
 
 let add = nums.reduce(function(acc , current , index , array) {
     console.log(`Acc => ${acc}`);
@@ -13,4 +13,23 @@ let add = nums.reduce(function(acc , current , index , array) {
     
 },5) // si on veut ajout 5 au debut de l'array
  
-console.log(add)
+console.log(add) */
+
+
+console.log("***********************************  Higher Order Functions -  Reduce Practice************************")
+
+///////////exercice remove @ from array and convert to string 
+let removeChart=["S","@","A","@","L","@","I","@","M"]
+
+let finalString = removeChart.filter(function(ele){
+    return !ele.startsWith('@')
+})/* .join('') */.reduce((acc , current)=>acc+current) // reduce qui va retourner 1 seul resultat
+console.log(finalString)
+
+/////////////// longest word
+let biggest =["salimbough", "salim" , "yassine" ,"ali"]
+
+let check = biggest.reduce(function(acc , current){
+    return acc.length > current.length ? acc : current
+})
+console.log(check);
