2a591247f3f2870cb26cce24ac90c221aa729a6d
salimboughanmi <boughanmi1salim@gmail.com>
2024-08-25 14:39:04 +0100

    Higher Order Functions - Map Pract

---
+++ jsElzero/function/HightOrderFCT.js
@@ -0,0 +1,16 @@
+/* 
+exemple with map MAP
+
+*/
+let swappingCases ='elzerO';
+let invertedNumbers =[1,-10 ,-20 ,15 ,20 ,100 ,-30];
+let ingnoreBooleans= "elz123ero" ;
+
+let swap = swappingCases.split("").map((function(element){
+    //condition ? true : false
+    return element === element.toUpperCase() ? element === element.toLowerCase() :element ===  element.toLowerCase() 
+})).join()
+
+console.log(swap)
+
+    
